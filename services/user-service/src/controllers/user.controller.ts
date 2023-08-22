520fca1ce85506b33f49d1a60b2916f7813f04c5
John Benedict Yan <johnbenedictyan@gmail.com>
2023-08-22 16:46:41 +0800

    Rearrange user serivce file structure

--- services/user-service/src/controllers/user.controller.ts
+++ services/user-service/src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
-import { userService } from "../../services/user.service";
+import { userService } from "../services/user.service";
 
 const createUser = async (req: Request, res: Response) => {
   const user = await userService.createUser(req.body);
