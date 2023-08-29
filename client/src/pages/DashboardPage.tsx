89b97ba5f9cd0817a83ae3c05136c9993bea6f30
John Benedict Yan <johnbenedictyan@gmail.com>
2023-08-29 17:25:56 +0800

    Update dashboard page

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,15 +1,9 @@
-import { Fragment, useState } from 'react'
-import { Dialog, Menu, Transition } from '@headlessui/react'
-import {
-    ArrowDownCircleIcon,
-    ArrowPathIcon,
-    ArrowUpCircleIcon,
-    Bars3Icon,
-    EllipsisHorizontalIcon,
-    PlusSmallIcon,
-} from '@heroicons/react/20/solid'
-import { BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { classNames } from '../util/ClassNames'
+import { Menu, Transition } from '@headlessui/react';
+import { ArrowDownCircleIcon, ArrowPathIcon, ArrowUpCircleIcon, EllipsisHorizontalIcon } from '@heroicons/react/20/solid';
+import { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { classNames } from '../util/ClassNames';
 
 const navigation = [
     { name: 'Home', href: '#' },
@@ -23,10 +17,9 @@ const secondaryNavigation = [
     { name: 'All-time', href: '#', current: false },
 ]
 const stats = [
-    { name: 'Revenue', value: '$405,091.00', change: '+4.75%', changeType: 'positive' },
-    { name: 'Overdue invoices', value: '$12,787.00', change: '+54.02%', changeType: 'negative' },
-    { name: 'Outstanding invoices', value: '$245,988.00', change: '-1.39%', changeType: 'positive' },
-    { name: 'Expenses', value: '$30,156.00', change: '+10.18%', changeType: 'negative' },
+    { name: 'Questions Attempted', value: '405', change: '+4.75%', changeType: 'positive' },
+    { name: 'Number of Matches', value: '12', change: '+54.02%', changeType: 'negative' },
+    { name: 'Acceptance Rate', value: '74.5%', change: '-1.39%', changeType: 'positive' },
 ]
 const statuses = {
     Paid: 'text-green-700 bg-green-50 ring-green-600/20',
@@ -120,7 +113,7 @@ const DashboardPage = () => {
                 {/* Secondary navigation */}
                 <header className="pb-4 pt-6 sm:pb-6">
                     <div className="mx-auto flex max-w-7xl flex-wrap items-center gap-6 px-4 sm:flex-nowrap sm:px-6 lg:px-8">
-                        <h1 className="text-base font-semibold leading-7 text-gray-900">Cashflow</h1>
+                        <h1 className="text-base font-semibold leading-7 text-gray-900">Statistics</h1>
                         <div className="order-last flex w-full gap-x-8 text-sm font-semibold leading-6 sm:order-none sm:w-auto sm:border-l sm:border-gray-200 sm:pl-6 sm:leading-7">
                             {secondaryNavigation.map((item) => (
                                 <a key={item.name} href={item.href} className={item.current ? 'text-indigo-600' : 'text-gray-700'}>
@@ -128,13 +121,13 @@ const DashboardPage = () => {
                                 </a>
                             ))}
                         </div>
-                        <a
+                        {/* <a
                             href="#"
                             className="ml-auto flex items-center gap-x-1 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
                             <PlusSmallIcon className="-ml-1.5 h-5 w-5" aria-hidden="true" />
                             New invoice
-                        </a>
+                        </a> */}
                     </div>
                 </header>
 
@@ -185,7 +178,7 @@ const DashboardPage = () => {
                 <div>
                     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                         <h2 className="mx-auto max-w-2xl text-base font-semibold leading-6 text-gray-900 lg:mx-0 lg:max-w-none">
-                            Recent activity
+                            Recent Questions
                         </h2>
                     </div>
                     <div className="mt-6 overflow-hidden border-t border-gray-100">
@@ -274,10 +267,10 @@ const DashboardPage = () => {
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
                         <div className="flex items-center justify-between">
-                            <h2 className="text-base font-semibold leading-7 text-gray-900">Recent clients</h2>
-                            <a href="#" className="text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                                View all<span className="sr-only">, clients</span>
-                            </a>
+                            <h2 className="text-base font-semibold leading-7 text-gray-900">Recent Matches</h2>
+                            <Link to={'/matches'} className="text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                                View all<span className="sr-only">, matches</span>
+                            </Link>
                         </div>
                         <ul role="list" className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
                             {clients.map((client) => (
