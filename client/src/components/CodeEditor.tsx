d17fcd2063726d11ceb6470958713a06f3e0642d
John Benedict Yan <johnbenedictyan@gmail.com>
2023-09-18 00:45:02 +0800

    Update code editor to have results

--- client/src/components/CodeEditor.tsx
+++ client/src/components/CodeEditor.tsx
@@ -4,13 +4,18 @@ import { cpp } from '@codemirror/lang-cpp';
 import { java } from '@codemirror/lang-java';
 import { javascript } from '@codemirror/lang-javascript';
 import { python } from '@codemirror/lang-python';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
+import CodeResult from './CodeResult';
 
 interface ICodeEditorProps {
     selectedLanguage: string;
 }
 
 const CodeEditor: React.FC<ICodeEditorProps> = ({ selectedLanguage }) => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [codeSubmitted, setCodeSubmitted] = useState<boolean>(false);
+    const [codeResult, setCodeResult] = useState<string>('');
+
     const languageMap = {
         'JavaScript': javascript,
         'Python': python,
@@ -22,21 +27,42 @@ const CodeEditor: React.FC<ICodeEditorProps> = ({ selectedLanguage }) => {
         console.log('value:', value);
     }, []);
 
+    const handleSubmit = () => {
+        console.log('submitting code');
+        setCodeResult('hello world!');
+        setCodeSubmitted(true);
+    }
+
     return (
-        <div className='h-144 border rounded-lg shadow'>
-            <CodeMirror
-                value="console.log('hello world!');"
-                height="576px"
-                extensions={[
-                    // Load all languages
-                    javascript(),
-                    python(),
-                    java(),
-                    cpp(),
-                ]}
-                onChange={onChange}
-            />
+        <div>
+            <div className='h-144 border rounded-lg shadow'>
+                <CodeMirror
+                    value="console.log('hello world!');"
+                    height="576px"
+                    extensions={[
+                        // Load all languages
+                        javascript(),
+                        python(),
+                        java(),
+                        cpp(),
+                    ]}
+                    onChange={onChange}
+                />
+            </div>
+            <div className="flex flex-row-reverse mt-5">
+                <button
+                    type="button"
+                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    onClick={handleSubmit}
+                >
+                    Submit Code
+                </button>
+            </div>
+            <div className="mt-5">
+                <CodeResult result={codeResult} />
+            </div>
         </div>
+
     )
 };
 
---
+++ client/src/components/CodeResult.tsx
@@ -0,0 +1,14 @@
+interface ICodeResultProps {
+    result: string;
+}
+
+const CodeResult: React.FC<ICodeResultProps> = ({ result }) => {
+    return (
+        <div className='h-80 border rounded-lg shadow p-5'>
+            <h1 className="font-bold">Code Result</h1>
+            <p>{result}</p>
+        </div>
+    )
+}
+
+export default CodeResult;
