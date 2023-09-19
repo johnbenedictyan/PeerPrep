2f4064b1944211477bd2c8c0e5fd747a0e69d84c
John Benedict Yan <johnbenedictyan@gmail.com>
2023-09-19 10:55:11 +0800

    Refactor matching controller tesst

--- services/matching-service/src/controllers/matching/matching.controller.test.ts
+++ services/matching-service/src/controllers/matching/matching.controller.test.ts
@@ -1,50 +1,53 @@
 import { getMockReq, getMockRes } from "@jest-mock/express";
-import {
-  afterEach,
-  beforeEach,
-  describe,
-  expect,
-  jest,
-  test,
-} from "@jest/globals";
+import { beforeEach, describe, expect, jest, test } from "@jest/globals";
+import { PrismaClient } from "@prisma/client";
 import httpStatus from "http-status";
+import { Kafka } from "kafkajs";
 import { IMatchingRequestCreateInput } from "../../interfaces/IMatching";
+import MatchingEventProducer from "../../kafka/producer/producer";
+import MatchingService from "../../services/matching/matching.service";
 import MatchingController from "./matching.controller";
+
+jest.mock("../../services/matching/matching.service");
+jest.mock("../../kafka/producer/producer");
+jest.mock("../../kafka/kafka");
+jest.mock("../../util/prisma/client");
+
+const mockService = jest.mocked(MatchingService);
+const mockPrisma = jest.mocked(PrismaClient);
+const mockKafka = jest.mocked(Kafka);
+const mockMatchingEventProducer = jest.mocked(MatchingEventProducer);
+
+const mockKafkaInstance = new mockKafka({
+  brokers: ["localhost:9092"],
+  clientId: "matching-service",
+});
+const mockMatchingEventProducerInstance = new mockMatchingEventProducer(
+  mockKafkaInstance
+);
+const mockPrismaInstance = new mockPrisma();
+const mockServiceInstance = new mockService(
+  mockMatchingEventProducerInstance,
+  mockPrismaInstance
+);
 
 describe("Test matching controller", () => {
   beforeEach(() => {
-    jest.mock("../services/matching.service");
-    jest.mock("../kafka/producer/producer");
-    jest.mock("../kafka/kafka");
-  });
-
-  afterEach(() => {
-    jest.restoreAllMocks();
+    mockService.mockClear();
+    mockMatchingEventProducer.mockClear();
   });
 
   test("Health Check should be 200", () => {
-    const kafka = require("../kafka/kafka").default;
-    const MatchingService = require("../services/matching.service").default;
-    const MatchingEventProducer = require("../kafka/producer/producer").default;
-
     const { res } = getMockRes({ locals: {} });
     const req = getMockReq({});
 
-    const ctrl = new MatchingController(
-      new MatchingService(new MatchingEventProducer(kafka))
-    );
+    const ctrl = new MatchingController(mockServiceInstance);
     ctrl.healthCheck(req, res);
 
     expect(res.json).toHaveBeenCalledWith(expect.stringMatching("OK"));
   });
 
   test("Happy Path: Complete Create Matching Request should be 200", async () => {
-    const mockedKafka = require("../kafka/kafka").default;
-    const mockedMatchingService =
-      require("../services/matching.service").default;
-    const mockedMatchingEventProducer =
-      require("../kafka/producer/producer").default;
-
     const completedMatchingRequestInput: IMatchingRequestCreateInput = {
       userId: "123",
       questionId: 12,
@@ -55,21 +58,13 @@ describe("Test matching controller", () => {
     const { res } = getMockRes({ locals: {} });
     const req = getMockReq({ body: completedMatchingRequestInput });
 
-    const controller = new MatchingController(
-      new mockedMatchingService(new mockedMatchingEventProducer(mockedKafka))
-    );
+    const controller = new MatchingController(mockServiceInstance);
     await controller.createMatchingRequest(req, res);
 
     expect(res.status).toHaveBeenCalledWith(httpStatus.CREATED);
   });
 
   test("Happy Path: No Optional Create Matching Request should be 200", async () => {
-    const mockedKafka = require("../kafka/kafka").default;
-    const mockedMatchingService =
-      require("../services/matching.service").default;
-    const mockedMatchingEventProducer =
-      require("../kafka/producer/producer").default;
-
     const completedMatchingRequestInput: IMatchingRequestCreateInput = {
       userId: "123",
       difficulty: "easy",
@@ -78,21 +73,13 @@ describe("Test matching controller", () => {
     const { res } = getMockRes({ locals: {} });
     const req = getMockReq({ body: completedMatchingRequestInput });
 
-    const controller = new MatchingController(
-      new mockedMatchingService(new mockedMatchingEventProducer(mockedKafka))
-    );
+    const controller = new MatchingController(mockServiceInstance);
     await controller.createMatchingRequest(req, res);
 
     expect(res.status).toHaveBeenCalledWith(httpStatus.CREATED);
   });
 
   test("Unhappy Path: User ID Missing Create Matching Request should be 400", async () => {
-    const mockedKafka = require("../kafka/kafka").default;
-    const mockedMatchingService =
-      require("../services/matching.service").default;
-    const mockedMatchingEventProducer =
-      require("../kafka/producer/producer").default;
-
     const requiredMissingMatchingRequestInput = {
       difficulty: "easy",
     };
@@ -100,21 +87,13 @@ describe("Test matching controller", () => {
     const { res } = getMockRes({ locals: {} });
     const req = getMockReq({ body: requiredMissingMatchingRequestInput });
 
-    const controller = new MatchingController(
-      new mockedMatchingService(new mockedMatchingEventProducer(mockedKafka))
-    );
+    const controller = new MatchingController(mockServiceInstance);
     await controller.createMatchingRequest(req, res);
 
     expect(res.status).toHaveBeenCalledWith(httpStatus.BAD_REQUEST);
   });
 
   test("Unhappy Path: Difficulty Missing Create Matching Request should be 400", async () => {
-    const mockedKafka = require("../kafka/kafka").default;
-    const mockedMatchingService =
-      require("../services/matching.service").default;
-    const mockedMatchingEventProducer =
-      require("../kafka/producer/producer").default;
-
     const requiredMissingMatchingRequestInput = {
       userId: "abc123",
     };
@@ -122,9 +101,7 @@ describe("Test matching controller", () => {
     const { res } = getMockRes({ locals: {} });
     const req = getMockReq({ body: requiredMissingMatchingRequestInput });
 
-    const controller = new MatchingController(
-      new mockedMatchingService(new mockedMatchingEventProducer(mockedKafka))
-    );
+    const controller = new MatchingController(mockServiceInstance);
     await controller.createMatchingRequest(req, res);
 
     expect(res.status).toHaveBeenCalledWith(httpStatus.BAD_REQUEST);
