5c645cbc555e936200ebbef3b6b5fb40b97cca16
John Benedict Yan <johnbenedictyan@gmail.com>
2023-11-08 23:17:30 +0800

    add themes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -110,9 +110,9 @@ function App() {
           <Route
             path="questions/:questionId"
             element={
-              <ProtectedRoute permissionRole="user">
-                <SingleQuestionPage />
-              </ProtectedRoute>
+              //   <ProtectedRoute permissionRole="user">
+              <SingleQuestionPage />
+              //   </ProtectedRoute>
             }
           />
           <Route path="*" element={<NotFoundPage />} />
